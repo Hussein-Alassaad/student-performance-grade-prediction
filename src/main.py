5888061deb4adb49539434dc74e3635703221891
Hussein-Alassaad <javahusseinyb@gmail.comL>
2026-06-06 01:39:23 +0300

    Explored dataset structure and target variable

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import pandas as pd
 df = pd.read_csv("Data/student_performance.csv", sep=";")
 
 print(df.head())
-
+print(df.shape)
+print(df.columns)
+print(df.info())
+print(df["G3"].describe())
 
 
