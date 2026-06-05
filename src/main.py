996ec73708278ea71be152cda05b4fb976cc119b
Hussein-Alassaad <javahusseinyb@gmail.comL>
2026-06-06 01:56:23 +0300

    Performed dataset exploration and target analysis

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("Data/student_performance.csv", sep=";")
 
@@ -7,5 +8,13 @@ print(df.shape)
 print(df.columns)
 print(df.info())
 print(df["G3"].describe())
+
+
+
+plt.hist(df["G3"], bins=20)
+plt.title("Distribution of Final Grades")
+plt.xlabel("G3")
+plt.ylabel("Number of Students")
+plt.show()   
 
 
