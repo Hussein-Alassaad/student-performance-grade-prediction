5f458e97dede6f2743e3e02738da57d7a32f5832
Hussein-Alassaad <javahusseinyb@gmail.comL>
2026-06-11 02:06:35 +0300

    Analyze relationships between features and final grade

--- src/main.py
+++ src/main.py
@@ -11,18 +11,28 @@ print(df["G3"].describe())
 
 
 
-plt.hist(df["G3"], bins=20)
+plt.hist(df["G3"], bins = 20 )
 plt.title("Distribution of Final Grades")
 plt.xlabel("G3")
-plt.ylabel("Number of Students")
-plt.show()   
+plt.ylabel("final grade")
+plt.show()
+
+
+
 
 plt.scatter(df["studytime"], df["G3"])
 
 plt.title("Study Time vs Final Grade")
 plt.xlabel("Study Time")
+plt.ylabel("Number of Students")
+plt.show()
+
+plt.scatter(df["G2"], df["G3"])
+
+
+plt.title("G2 vs Final Grade")
+plt.xlabel("G2")
 plt.ylabel("Final Grade (G3)")
 
 plt.show()
-
 
