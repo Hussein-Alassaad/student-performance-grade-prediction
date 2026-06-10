b40bc2b553db1e81ea56d9d3ff7db51be0690a8e
Hussein-Alassaad <javahusseinyb@gmail.comL>
2026-06-11 02:46:07 +0300

    Split data into training and testing sets

--- src/main.py
+++ src/main.py
@@ -53,8 +53,16 @@ from sklearn.metrics import r2_score
 
 x = df[["G1" , "G2"]]
 y = df["G3"]
-print(x.head())
-print(y.head())
+#print(x.head())
+#print(y.head())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
 
 
 
